1c3cbf04a5e50e2f80770ec195abb472f8860bdf
shirtydiamond33 <kyrixty@gmail.com>
2026-07-14 19:04:20 -0400

    fix trailing comma issues with __VA_OPT__

--- include/xcerror.h
+++ include/xcerror.h
@@ -10,11 +10,11 @@
 #define XCS_ERR_CODE_LEXER 255          // Lexer error code
 #define XCS_ERR_CODE_PARSER 254         // Parser error code
 #define __XCS_ERROR(CODE, FMT, ...) \
-    fprintf(stderr, "\033[31mXCS: \033[0m (line: %d) "FMT"\n", __LINE__, __VA_ARGS__);    \
+    fprintf(stderr, "\033[31mXCS: \033[0m (line: %d) "FMT"\n", __LINE__ __VA_OPT__(,) __VA_ARGS__);    \
     exit(CODE);
 
-#define XCS_ERROR(FMT, ...)         __XCS_ERROR(XCS_ERR_CODE_GENERIC, FMT, __VA_ARGS__);
-#define XCS_LEXER_ERROR(FMT, ...)   __XCS_ERROR(XCS_ERR_CODE_LEXER, "[Lexer]\t"FMT, __VA_ARGS__);
-#define XCS_PARSER_ERROR(FMT, ...)  __XCS_ERROR(XCS_ERR_CODE_PARSER, "[Parser]\t"FMT, __VA_ARGS__);
+#define XCS_ERROR(FMT, ...)         __XCS_ERROR(XCS_ERR_CODE_GENERIC, FMT __VA_OPT__(,) __VA_ARGS__);
+#define XCS_LEXER_ERROR(FMT, ...)   __XCS_ERROR(XCS_ERR_CODE_LEXER, "[Lexer]\t"FMT __VA_OPT__(,) __VA_ARGS__);
+#define XCS_PARSER_ERROR(FMT, ...)  __XCS_ERROR(XCS_ERR_CODE_PARSER, "[Parser]\t"FMT __VA_OPT__(,) __VA_ARGS__);
 
 #endif
